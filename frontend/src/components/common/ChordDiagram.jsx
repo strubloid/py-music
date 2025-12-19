f7073094b7529b10b303a6a0b95329c23039b6f1
Strubloid <strubloid@gmail.com>
2025-12-19 22:25:00 +0000

    fixing the chords fingers

--- frontend/src/components/common/ChordDiagram.jsx
+++ frontend/src/components/common/ChordDiagram.jsx
@@ -3,31 +3,42 @@ import './ChordDiagram.css'
 
 // Guitar chord fingering data
 const CHORD_FINGERINGS = {
-  'C': { frets: [0, 1, 0, 2, 3, 0], fingers: [0, 1, 0, 2, 3, 0], name: 'C Major' },
-  'D': { frets: [2, 2, 0, 2, 3, 2], fingers: [1, 1, 0, 2, 3, 1], name: 'D Major' },
+  // Major chords
+  'C': { frets: [-1, 3, 2, 0, 1, 0], fingers: [0, 3, 2, 0, 1, 0], name: 'C Major' },
+  'D': { frets: [-1, -1, 0, 2, 3, 2], fingers: [0, 0, 0, 1, 3, 2], name: 'D Major' },
   'E': { frets: [0, 2, 2, 1, 0, 0], fingers: [0, 2, 3, 1, 0, 0], name: 'E Major' },
   'F': { frets: [1, 3, 3, 2, 1, 1], fingers: [1, 4, 4, 3, 1, 1], name: 'F Major' },
   'G': { frets: [3, 2, 0, 0, 3, 3], fingers: [3, 2, 0, 0, 3, 4], name: 'G Major' },
-  'A': { frets: [0, 0, 2, 2, 2, 0], fingers: [0, 0, 1, 2, 3, 0], name: 'A Major' },
-  'B': { frets: [2, 2, 4, 4, 4, 2], fingers: [1, 1, 2, 3, 4, 1], name: 'B Major' },
+  'A': { frets: [-1, 0, 2, 2, 2, 0], fingers: [0, 0, 1, 2, 3, 0], name: 'A Major' },
+  'B': { frets: [-1, 2, 4, 4, 4, 2], fingers: [0, 1, 2, 3, 4, 1], name: 'B Major' },
   
   // Minor chords
-  'Cm': { frets: [0, 1, 3, 3, 2, 0], fingers: [0, 1, 4, 3, 2, 0], name: 'C Minor' },
-  'Dm': { frets: [1, 0, 0, 2, 3, 1], fingers: [1, 0, 0, 2, 3, 1], name: 'D Minor' },
+  'Cm': { frets: [-1, 3, 5, 5, 4, 3], fingers: [0, 1, 3, 4, 2, 1], name: 'C Minor' },
+  'Dm': { frets: [-1, -1, 0, 2, 3, 1], fingers: [0, 0, 0, 1, 3, 2], name: 'D Minor' },
   'Em': { frets: [0, 2, 2, 0, 0, 0], fingers: [0, 2, 3, 0, 0, 0], name: 'E Minor' },
   'Fm': { frets: [1, 3, 3, 1, 1, 1], fingers: [1, 3, 4, 1, 1, 1], name: 'F Minor' },
-  'Gm': { frets: [3, 1, 0, 0, 3, 3], fingers: [3, 1, 0, 0, 3, 4], name: 'G Minor' },
-  'Am': { frets: [0, 0, 2, 2, 1, 0], fingers: [0, 0, 2, 3, 1, 0], name: 'A Minor' },
-  'Bm': { frets: [2, 2, 4, 4, 3, 2], fingers: [1, 1, 3, 4, 2, 1], name: 'B Minor' },
+  'Gm': { frets: [3, 5, 5, 3, 3, 3], fingers: [1, 3, 4, 1, 1, 1], name: 'G Minor' },
+  'Am': { frets: [-1, 0, 2, 2, 1, 0], fingers: [0, 0, 2, 3, 1, 0], name: 'A Minor' },
+  'Bm': { frets: [-1, 2, 4, 4, 3, 2], fingers: [0, 1, 3, 4, 2, 1], name: 'B Minor' },
   
   // 7th chords
-  'C7': { frets: [0, 1, 0, 2, 1, 0], fingers: [0, 1, 0, 3, 2, 0], name: 'C Dominant 7' },
-  'D7': { frets: [2, 0, 0, 2, 1, 2], fingers: [2, 0, 0, 3, 1, 2], name: 'D Dominant 7' },
+  'C7': { frets: [-1, 3, 2, 3, 1, 0], fingers: [0, 3, 2, 4, 1, 0], name: 'C Dominant 7' },
+  'D7': { frets: [-1, -1, 0, 2, 1, 2], fingers: [0, 0, 0, 2, 1, 3], name: 'D Dominant 7' },
   'E7': { frets: [0, 2, 0, 1, 0, 0], fingers: [0, 2, 0, 1, 0, 0], name: 'E Dominant 7' },
   'F7': { frets: [1, 3, 1, 2, 1, 1], fingers: [1, 4, 2, 3, 1, 1], name: 'F Dominant 7' },
   'G7': { frets: [3, 2, 0, 0, 0, 1], fingers: [3, 2, 0, 0, 0, 1], name: 'G Dominant 7' },
-  'A7': { frets: [0, 0, 2, 0, 2, 0], fingers: [0, 0, 2, 0, 3, 0], name: 'A Dominant 7' },
-  'B7': { frets: [2, 0, 1, 2, 0, 2], fingers: [2, 0, 1, 3, 0, 4], name: 'B Dominant 7' }
+  'A7': { frets: [-1, 0, 2, 0, 2, 0], fingers: [0, 0, 2, 0, 3, 0], name: 'A Dominant 7' },
+  'B7': { frets: [-1, 2, 1, 2, 0, 2], fingers: [0, 2, 1, 3, 0, 4], name: 'B Dominant 7' },
+  
+  // Additional useful chord variations
+  'Cadd9': { frets: [-1, 3, 2, 0, 3, 0], fingers: [0, 2, 1, 0, 3, 0], name: 'C add9' },
+  'Dsus2': { frets: [-1, -1, 0, 2, 3, 0], fingers: [0, 0, 0, 1, 2, 0], name: 'D sus2' },
+  'Dsus4': { frets: [-1, -1, 0, 2, 3, 3], fingers: [0, 0, 0, 1, 2, 3], name: 'D sus4' },
+  'Esus4': { frets: [0, 2, 2, 2, 0, 0], fingers: [0, 1, 2, 3, 0, 0], name: 'E sus4' },
+  'Fsus2': { frets: [1, 3, 3, 0, 1, 1], fingers: [1, 3, 4, 0, 1, 1], name: 'F sus2' },
+  'Gsus4': { frets: [3, 3, 0, 0, 3, 3], fingers: [2, 3, 0, 0, 2, 4], name: 'G sus4' },
+  'Asus2': { frets: [-1, 0, 2, 2, 0, 0], fingers: [0, 0, 1, 2, 0, 0], name: 'A sus2' },
+  'Asus4': { frets: [-1, 0, 2, 2, 3, 0], fingers: [0, 0, 1, 2, 3, 0], name: 'A sus4' }
 }
 
 const ChordDiagram = ({ chord, size = 'medium' }) => {
@@ -98,7 +109,29 @@ const ChordDiagram = ({ chord, size = 'medium' }) => {
           const x = 10 + stringIndex * stringSpacing
           const finger = chordData.fingers[stringIndex]
           
-          if (fret === 0) {
+          if (fret === -1) {
+            // Muted string - X above nut
+            return (
+              <g key={stringIndex}>
+                <line
+                  x1={x - 3}
+                  y1="8"
+                  x2={x + 3}
+                  y2="16"
+                  stroke="#ff4444"
+                  strokeWidth="2"
+                />
+                <line
+                  x1={x - 3}
+                  y1="16"
+                  x2={x + 3}
+                  y2="8"
+                  stroke="#ff4444"
+                  strokeWidth="2"
+                />
+              </g>
+            )
+          } else if (fret === 0) {
             // Open string - circle above nut
             return (
               <circle
