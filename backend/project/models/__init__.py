03907a141503fb15e79e9b582c65784c21bf2cef
Strubloid <strubloid@gmail.com>
2026-07-07 20:39:59 +0100

    updating the fly.io db fixes

--- backend/project/models/__init__.py
+++ backend/project/models/__init__.py
@@ -3,3 +3,7 @@ from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+# Import all model classes so they register with SQLAlchemy before
+# db.create_all() is called in app.py at import time.
+from .user import User, Progression, Favorite, ChallengeAttempt
